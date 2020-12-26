048bee7b036a696576c62cf7ff0367bc0e66f542
gwuah <griffithawuah15@gmail.com>
2020-12-26 21:42:43 +0000

    added logger to code

--- routes/link.go
+++ routes/link.go
@@ -3,13 +3,14 @@ package routes
 import (
 	"brank/core"
 	"brank/services"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterLinkRoutes(e *gin.Engine, s services.Services) {
-
+	log.Println("nothing to commit, working tree clean")
 	e.POST("/exchange", func(c *gin.Context) {
 		var req core.ExchangeContractCode
 		if err := c.ShouldBindJSON(&req); err != nil {
